5a9597aa3337bc262edd8550fcef738776d8414c
A.Junior <junior.huancas.leuyacc@gmail.com>
2024-09-08 20:59:19 -0500

    login - final version

--- migrations/Version20240908235413.php
+++ migrations/Version20240908235413.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240908221140 extends AbstractMigration
+final class Version20240908235413 extends AbstractMigration
 {
     public function getDescription(): string
     {
